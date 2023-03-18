0821864b5881f6a2ecf949ea56f5b48d8498f7b1
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-18 17:01:11 -0400

    removed foreign key constaint during the migration to prevent 'missing value' problem

--- database/migrations/2023_03_01_232642_create_etudiants_table.php
+++ database/migrations/2023_03_01_232642_create_etudiants_table.php
@@ -6,32 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateEtudiantsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('etudiants', function (Blueprint $table) {
-            $table->id();
-            $table->string('nom', 50);
-            $table->string('adresse', 100);
-            $table->string('phone', 20);
-            $table->string('email', 50);
-            $table->date('date_naissance', 50);
-            $table->foreignId('ville_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('etudiants');
-    }
+    Schema::create('etudiants', function (Blueprint $table) {
+      $table->id();
+      $table->string('nom', 50);
+      $table->string('adresse', 100);
+      $table->string('phone', 20);
+      $table->string('email', 50);
+      $table->date('date_naissance');
+      $table->foreignId('ville_id')->constrained()->onDelete('cascade');
+      $table->timestamps();
+    });
+
+    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('etudiants');
+  }
 }
