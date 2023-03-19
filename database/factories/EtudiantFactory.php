150f50ac7946cdb709c2fbf648e4c1c24a3a4270
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-18 23:47:07 -0400

    optimisation de la création de données (faker) avec clé étrangère

--- database/factories/EtudiantFactory.php
+++ database/factories/EtudiantFactory.php
@@ -3,23 +3,25 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Ville;
 
 class EtudiantFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'nom' => $this->faker->name(),
-            'adresse' => $this->faker->address(),
-            'phone' => $this->faker->phoneNumber(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'date_naissance' => $this->faker->date($format = 'Y-m-d', $max = '2005-12-31'),
-            'ville_id' => $this->faker->numberBetween($min = 1, $max = 15)
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition()
+  {
+    $ville = Ville::inRandomOrder()->first(); // get a random Ville record from the database
+    return [
+      'nom' => $this->faker->name(),
+      'adresse' => $this->faker->address(),
+      'phone' => $this->faker->phoneNumber(),
+      'email' => $this->faker->unique()->safeEmail(),
+      'date_naissance' => $this->faker->date($format = 'Y-m-d', $max = '2005-12-31'),
+      'ville_id' => $ville->id
+    ];
+  }
 }
