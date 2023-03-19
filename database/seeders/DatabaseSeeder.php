45eb27e78c4f21cec5b1bb3bdcebe42bb6974a63
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-18 23:46:23 -0400

    création de fonctions pour chacun des seeder (plus maintenable à long terme)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,31 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+    public function run()
+    {
+        $this->call([
+            VilleTableSeeder::class,
+            EtudiantTableSeeder::class,
+        ]);
+    }
+}
+
+class VilleTableSeeder extends Seeder
+{
     public function run()
     {
         \App\Models\Ville::factory(15)->create();
+    }
+}
+
+class EtudiantTableSeeder extends Seeder
+{
+    public function run()
+    {
         \App\Models\Etudiant::factory(100)->create();
-        // \App\Models\User::factory(10)->create();
     }
 }
