dd29be615736c9350be70dbfb93acf68026fbc69
Rizo <g.rizzoli@hotmail.ca>
2023-03-21 17:23:00 -0400

    mise en place d'un mecanisme rollback si la création de l'étudiant ne fonctionne pas afin d'éviter un décallage entre user et étudiant

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\Etudiant;
 use App\Models\Ville;
 use App\Models\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 
@@ -41,19 +42,30 @@ class UserController extends Controller
    */
   public function store(Request $request)
   {
-    $user = User::create([
-      'email' => $request->email,
-      'password' => Hash::make($request->password)
-    ]);
 
-    $etudiant = Etudiant::with('user')->create([
-      'user_id' => $user['id'],
-      'nom' => $request->nom,
-      'ville_id' => $request->ville_id,
-      'adresse' => $request->adresse,
-      'date_naissance' => $request->date_naissance,
-      'phone' => $request->phone
-    ]);
+    // si la création d'étudiant échoue, je veux qu'user soit rollback, et vice-versa
+    DB::beginTransaction();
+
+    try {
+
+      $user = User::create([
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+      ]);
+      
+      $etudiant = Etudiant::with('user')->create([
+        'user_id' => $user['id'],
+        'nom' => $request->nom,
+        'ville_id' => $request->ville_id,
+        'adresse' => $request->adresse,
+        'date_naissance' => $request->date_naissance,
+        'phone' => $request->phone
+      ]);
+      DB::commit(); // ça marche!
+    } catch (\Exception $e) {
+      DB::rollback(); // Abort! Abort!
+      throw $e;
+    }
 
     return redirect(route('etudiant.show', $etudiant->user_id));
   }
