8d8b70be08da59a662d5a5ce77e754af7bd2e7d7
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-22 23:43:04 -0400

    ajout de message de succes lors de la modification

--- resources/views/etudiant/index.blade.php
+++ resources/views/etudiant/index.blade.php
@@ -4,6 +4,14 @@
 @section('content')
 
   <div class="col-12">
+    @if (session('success'))
+      <div class="card-header">
+        <div class="alert alert-warning alert-dismissible fade show text-light" role="alert">
+          <strong>{{ session('success') }}</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </div>
+    @endif
     <table class="table table-dark table-striped table-hover">
       <thead class="table-primary">
         <tr>
@@ -21,7 +29,7 @@
             <td class="text-center"><a href="{{ route('etudiant.show', $etudiant->user_id) }}"
                 class="text-decoration-none link-light">{{ $etudiant->nom }}</a></td>
             <td class="text-center"><a href="{{ route('etudiant.show', $etudiant->user_id) }}"
-                class="text-decoration-none link-light">{{ $etudiant->user->email }}</a></td>
+                class="text-decoration-none link-light">{{ $etudiant->etudiantBelongsToUser->email }}</a></td>
             <td class="text-center"><a href="{{ route('etudiant.show', $etudiant->user_id) }}"
                 class="text-decoration-none link-light">{{ $etudiant->date_naissance }}</a></td>
             <td class="text-center"><a href="{{ route('etudiant.edit', $etudiant->user_id) }}"
@@ -29,14 +37,11 @@
                 <i class="fa-solid fa-rotate-right text-primary"></i>
               </a></td>
             <td class="text-center">
-              <form action="{{ route('etudiant.delete', $etudiant->user_id) }}" method="POST"
-                class="text-decoration-none link-light">
-                @csrf
-                @method('delete')
-                <button type="submit" class="btn-unstyled">
+              <div class="text-decoration-none link-light">
+                <button type="button" class="btn-unstyled" data-bs-toggle="modal" data-bs-target="#exampleModal">
                   {!! $icon !!}
                 </button>
-              </form>
+              </div>
             </td>
           </tr>
 
@@ -45,8 +50,29 @@
         @endforelse
       </tbody>
     </table>
-  </div>
-  <div class="py-5">
-    {{ $pagination->links('layouts.pagination') }}
-  </div>
-@endsection
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Attention!</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Êtes-vous sûr que vous souhaitez retirer {{ $etudiant->nom }} de la liste des étudiants?
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Oh non!</button>
+            <form action="{{ route('article.delete', $etudiant) }}" method="post">
+              @csrf
+              @method('delete')
+              <input type="submit" class="btn btn-danger" value="Sûr comme un citron">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="py-5">
+      {{ $pagination->links('layouts.pagination') }}
+    </div>
+  @endsection
