ebb1db7099f5487f13cd26392d1f0ac5a43c205d
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-23 11:22:06 -0400

    lexique français

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Les informations saisies ne correspondent pas à celles de nos données.',
+    'password' => 'Le mot de passe est incorrect.',
+    'throttle' => 'Vous avez essayé de vous connecter trop de fois. Veuillez réessayer dans :seconds seconds.',
+
+];
--- resources/lang/fr/lang.php
+++ resources/lang/fr/lang.php
@@ -1,9 +1,78 @@
 <?php
 return [
+
+  // general
  'register_user' => 'Enregistrer un utilisateur',
+ 'success' => 'Succès',
  'name' => 'Nom',
  'email' => 'Courriel',
  'password' => 'Mot de passe',
  'sign_up' => 'Enregistrer',
- 'our_students' => 'Nos étudiants',
+ 'submit' => 'Allez hop, cascade',
+ 'delete' => 'Supprimer',
+ 'back' => 'Retour',
+ 'warning' => 'Attention',
+ 'no' => 'Oh non',
+ 'confirm' => 'Sûr comme un citron',
+
+
+  // menu
+  'our_students' => 'Nos étudiants',
+  'admission' => 'Admission',
+  'login' => 'Se connecter',
+  'articles' => 'Articles',
+  'my_articles' => 'Mes articles',
+  'write' => 'Écrire',
+  'logout' => 'Se déconnecter',
+  'copyrights' => '2023 Tous les droits réservés ',
+  'hello' => 'Bonjour',
+  'guest' => 'cher',
+  'admission' => 'Admission',
+
+  // article
+  'be_creative' => 'Soyez créatif',
+  'content' => 'Contenu',
+  'back_to_user_articles' => 'Retour à mes articles',
+  'title' => 'Titre',
+  'language' => 'Langue',
+  'author' => 'Auteur',
+  'extract' => 'Extrait',
+  'creation_date' => 'Date de création',
+  'modification_date' => 'Dernière modification',
+  'no_article' => "Pas d'article",
+  'my_articles' => 'Mes articles',
+  'edit' => 'Modifier',
+  'confirm_article_delete' => 'Êtes-vous sûr que vous souhaitez supprimer définitivement votre article ',
+  'the_articles' => 'Les articles',
+  'modification_article' => "Modification d'article",
+
+  // welcome
+  'welcome' => 'Bienvenue',
+  'home' => 'Accueil',
+
+
+  'are_you_sure' => 'Êtes-vous sûr que vous souhaitez retirer',
+  'student_list' => ' de la liste des étudiants',
+  
+  // login
+  'provide_infos' => 'Veuillez saisir vos informations',
+  'email' => 'Courriel',
+  'password' => 'Mot de passe',
+  'edit' => 'Modifier',
+  'no_student' => 'Aucun étudiant sélectionné',
+
+  // compte
+  'name' => 'Nom',
+  'phone' => 'Téléphone',
+  'birthday' => 'Date de naissance',
+  'city' => 'Ville',
+  // errors
+  '404_message' => '404 Error: Page Not Found',
+  '404_description' => 'The page you are looking for could not be found.',
+  'page_not_found' => "La page n'a pas été trouvée",
+  'server_error' => 'Server Error',
+  '500_message' => '500 Error: Server Error',
+  '500_description' => 'There was an error processing your request. Please try again later.',
+  // errors
+  
 ];
