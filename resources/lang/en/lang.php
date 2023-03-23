0e7a8297224d3a3c5ad6c8fcb7f7aa6eb722b09a
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-23 11:52:00 -0400

    lexique anglais

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -1,9 +1,68 @@
 <?php
 return [
- 'register_user' => 'Register User',
- 'name' => 'Name',
- 'email' => 'Email',
- 'password' => 'Password',
- 'sign_up' => 'Sign up',
- 'our_students' => 'Our students',
+
+  // general
+  'welcome' => 'Welcome',
+  'success' => 'Success',
+  'edit' => 'Edit',
+  'submit' => "It's a go",
+  'delete' => 'Delete',
+  'back' => 'Back',
+  'warning' => 'Attention',
+  'are_you_sure' => 'Are you sure you want to remove',
+  'no' => 'Mhh...',
+  'confirm' => 'Like a lemon',
+
+  // user
+  'register_user' => 'Register User',
+  'email' => 'Email',
+  'password' => 'Password',
+  'sign_up' => 'Sign up',
+  'provide_infos' => 'Please enter your informations',
+
+  // students
+  'name' => 'Name',
+  'phone' => 'Phone',
+  'birthday' => 'Birthday',
+  'city' => 'City',
+  'no_student' => 'No student selected',
+  'student_list' => ' from the student list',
+  
+  // menu
+  'home' => 'Home',
+  'our_students' => 'Our students',
+  'admission' => 'Admission',
+  'login' => 'Login',
+  'articles' => 'Articles',
+  'my_articles' => 'My articles',
+  'write' => 'Write',
+  'logout' => 'Log out',
+  'copyrights' => '2023 All rights reserved ',
+  'hello' => 'Hello',
+  'guest' => 'dear',
+  
+
+  // article
+  'be_creative' => 'Be creative',
+  'content' => 'Content',
+  'back_to_user_articles' => 'Back to my articles',
+  'title' => 'Title',
+  'language' => 'Language',
+  'author' => 'Author',
+  'extract' => 'Extract',
+  'creation_date' => 'Creation date',
+  'modification_date' => 'Last modification',
+  'no_article' => "No article",
+  'confirm_article_delete' => 'Are you sure you want to definitively delete your article ',
+  'the_articles' => 'The articles',
+  'modification_article' => "Edit your article",
+
+
+  // errors
+  '404_message' => '404 Error: Page Not Found',
+  '404_description' => 'The page you are looking for could not be found.',
+  'page_not_found' => "The page could not be found",
+  'server_error' => 'Server Error',
+  '500_message' => '500 Error: Server Error',
+  '500_description' => 'There was an error processing your request. Please try again later.',
 ];
