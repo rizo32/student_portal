baa9cf91894e6d08543e68efc7117a21c47eaee2
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-24 23:52:46 -0400

    création du modèle Language

---
+++ app/Models/Language.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+  use HasFactory;
+
+  protected $primaryKey = 'id';
+
+  protected $fillable = [
+    'name',
+  ];
+
+  /**
+   * Get the article languages for the language.
+   */
+  public function articleLanguages()
+  {
+    return $this->hasMany(ArticleLanguage::class);
+  }
+
+  /**
+   * Get the article languages for the language.
+   */
+  public function articles()
+  {
+    return $this->hasMany(articles::class);
+  }
+
+}
