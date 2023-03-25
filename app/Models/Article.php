adfc53592bc5921284827340ea86f6540ad25a33
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-24 23:50:20 -0400

    ajout des relations avec 'language' et 'articleLanguages'

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -12,12 +12,25 @@ class Article extends Model
     protected $fillable = [
       'title',
       'body',
+      'title_fr',
+      'body_fr',
       'user_id',
       'language'
   ];
 
-  public function articleBelongsToUser()
+  public function user()
   {
     return $this->belongsTo(User::class, 'user_id');
+  }
+
+
+  public function language()
+  {
+    return $this->belongsTo(Language::class);
+  }
+
+  public function articleLanguages()
+  {
+    return $this->hasMany(User::class);
   }
 }
