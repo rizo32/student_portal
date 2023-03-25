0db96b3873b7b582948bc2982051537dde4da78d
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-24 23:51:58 -0400

    adaptation du code de M. Sanches pas certain que je vais vraiment utiliser la classe pour le tp

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-class Categorie extends Model
+
+class Category extends Model
 {
-//si la table n'a pas de "s" à la fin ou n'est pas le meme nom
-protected $table = "categorie";
-// si vous n'avez pas de created_at et updated_at champs.
-public $timestamp = false;
-use HasFactory;
-static public function selectCategorie(){
-$lg=null;
-if(session()->has('locale') && session()->get('locale') == 'fr'){
-$lg = "_fr";
-}
-$query = Categorie::Select('id',
-DB::raw('(case when categorie'.$lg.' is null then categorie else categorie'.$lg.' end) as categorie'))
-->OrderBy('categorie')
-->get();
-return $query;
-}
+  protected $table = "categorie";
+
+  public $timestamp = false;
+  use HasFactory;
+  static public function selectCategory()
+  {
+    $lang = session()->get('localeDB');
+
+    return Category::select('id', DB::raw("(CASE WHEN category$lang IS NULL THEN category ELSE category$lang END) as category"))->orderby('category')->get();
+  }
 }
