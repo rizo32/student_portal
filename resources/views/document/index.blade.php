da0d9cddac5c100c27725dc3fe4a375ba9dd6ca5
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-04-04 12:37:27 -0400

    create index document page

--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
-@section('title', trans('lang.our_students'))
-@section('header', trans('lang.our_students'))
+@section('title', trans('lang.documents'))
+@section('header', trans('lang.documents'))
 @section('content')
 
   <div class="col-12">
@@ -12,54 +12,80 @@
         </div>
       </div>
     @endif
+    @if (session('errors'))
+    <div class="card-header">
+      <div class="alert alert-danger alert-dismissible fade show text-dark" role="alert">
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
+    @endif
     <table class="table table-dark table-striped table-hover">
       <thead class="table-primary">
         <tr>
-          <th class="text-center text-dark">@lang('lang.name')</th>
-          <th class="text-center text-dark">@lang('lang.email')</th>
-          <th class="text-center text-dark">@lang('lang.birthday')</th>
-          <th class="text-center text-dark">@lang('lang.edit')</th>
-          <th class="text-center text-dark">@lang('lang.delete')</th>
+          <th class="text-center text-dark">@lang('lang.title')</th>
+          <th class="text-center text-dark">@lang('lang.format')</th>
+          <th class="text-center text-dark">@lang('lang.creation_date')</th>
+          <th class="text-center text-dark">@lang('lang.modification_date')</th>
+          <th class="text-center text-dark">@lang('lang.download')</th>
         </tr>
       </thead>
       <tbody>
 
-        @forelse($students as $student)
+        @php
+          $lang_local = app()->getLocale();
+          $lang_alt = $lang_local === 'en' ? 'fr' : 'en';
+        @endphp
+
+        @forelse($documents as $document)
           <tr>
-            <td class="text-center"><a href="{{ route('student.show', $student->user_id) }}"
-                class="text-decoration-none link-light">{{ $student->name }}</a></td>
-            <td class="text-center"><a href="{{ route('student.show', $student->user_id) }}"
-                class="text-decoration-none link-light">{{ $student->user->email }}</a></td>
-            <td class="text-center"><a href="{{ route('student.show', $student->user_id) }}"
-                class="text-decoration-none link-light">{{ $student->birthday }}</a></td>
-            <td class="text-center"><a href="{{ route('student.edit', $student->user_id) }}"
-                class="text-decoration-none link-light">
-                <i class="fa-solid fa-rotate-right text-primary"></i>
-              </a></td>
             <td class="text-center">
-              <div class="text-decoration-none link-light">
-                <button type="button" class="btn-unstyled" data-bs-toggle="modal"
-                  data-bs-target="#deleteModal{{ $student->user_id }}">
-                  {!! $iconHtml !!}
-                </button>
-              </div>
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                @if (isset($document->{'title_' . $lang_local}))
+                  {{ $document->{'title_' . $lang_local} }}
+                @else
+                  {{ __('lang.no_title') }}
+                @endif
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->format->name }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->created_at }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->updated_at }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.download', $document->id) }}" class="text-decoration-none link-light">
+                <i class="fa-solid fa-arrow-down text-primary"></i>
+              </a>
             </td>
           </tr>
           <!-- Modal -->
-          <div class="modal fade" id="deleteModal{{ $student->user_id }}" tabindex="-1"
-            aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="deleteModal{{ $document->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
                   <h1 class="modal-title fs-5" id="exampleModalLabel">@lang('lang.warning')!</h1>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">@lang('lang.are_you_sure')
-                  {{ $student->name }}@lang('lang.student_list')?
+                <div class="modal-body">
+                  @lang('lang.confirm_document_delete'){{ $document->title }}?
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">@lang('lang.no')!</button>
-                  <form action="{{ route('student.delete', $student) }}" method="post">
+                  <form action="{{ route('document.delete', $document->id) }}" method="post">
                     @csrf
                     @method('delete')
                     <input type="submit" class="btn btn-danger" value="@lang('lang.confirm')">
@@ -67,14 +93,15 @@
                 </div>
               </div>
             </div>
-          </div>
-
-        @empty
-          <tr>@lang('lang.no_student')</tr>
+          @empty
+            <tr>@lang('lang.no_document')</tr>
         @endforelse
       </tbody>
     </table>
-    <div class="py-5">
-      {{ $pagination->links('layouts.pagination') }}
-    </div>
-  @endsection
+  </div>
+
+
+  <div class="py-5">
+    {{ $pagination->links('layouts.pagination') }}
+  </div>
+@endsection
