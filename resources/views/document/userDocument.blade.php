f2d1a93db27d6f56938407e615d89505012558af
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-04-03 23:57:14 -0400

    added error modal, finished view

--- resources/views/document/userDocument.blade.php
+++ resources/views/document/userDocument.blade.php
@@ -12,81 +12,104 @@
         </div>
       </div>
     @endif
+    @if (session('errors'))
+    <div class="card-header">
+      <div class="alert alert-danger alert-dismissible fade show text-dark" role="alert">
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+    </div>
+    @endif
     <table class="table table-dark table-striped table-hover">
       <thead class="table-primary">
         <tr>
           <th class="text-center text-dark">@lang('lang.title')</th>
+          <th class="text-center text-dark">@lang('lang.format')</th>
           <th class="text-center text-dark">@lang('lang.creation_date')</th>
           <th class="text-center text-dark">@lang('lang.modification_date')</th>
-          <th class="text-center text-dark">@lang('lang.language')</th>
+          <th class="text-center text-dark">@lang('lang.download')</th>
           <th class="text-center text-dark">@lang('lang.edit')</th>
           <th class="text-center text-dark">@lang('lang.delete')</th>
         </tr>
       </thead>
       <tbody>
 
-        @forelse($articles as $article)
-          @foreach ($article->articleLanguage as $language)
-            <tr>
-              <td class="text-center">
-                <a href="{{ route('article.show', [$article->id, $language->language->id]) }}" class="text-decoration-none link-light">
-                  {{ $language->title }}
-                </a>
-              </td>
-              <td class="text-center">
-                <a href="{{ route('article.show', [$article->id, $language->language->id]) }}" class="text-decoration-none link-light">
-                  {{ $language->created_at }}
-                </a>
-              </td>
-              <td class="text-center">
-                <a href="{{ route('article.show', [$article->id, $language->language->id]) }}" class="text-decoration-none link-light">
-                  {{ $language->updated_at }}
-                </a>
-              </td>
-              <td class="text-center">
-                <a href="{{ route('article.show', [$article->id, $language->language->id]) }}" class="text-decoration-none link-light">
-                  {{ $language->language->name }}
-                </a>
-              </td>
-              <td class="text-center"><a href="{{ route('article.edit', [$article->id, $language->language->id]) }}"
-                  class="text-decoration-none link-light">
-                  <i class="fa-solid fa-rotate-right text-primary"></i>
-                </a></td>
-              <td class="text-center">
-                <div class="text-decoration-none link-light">
-                  <button type="button" class="btn-unstyled" data-bs-toggle="modal"
-                    data-bs-target="#deleteModal{{ $article->id }}">
-                    {!! $icon !!}
-                  </button>
-                </div>
+        @php
+          $lang_local = app()->getLocale();
+          $lang_alt = $lang_local === 'en' ? 'fr' : 'en';
+        @endphp
 
-              </td>
-            </tr>
-            <!-- Modal -->
-            <div class="modal fade" id="deleteModal{{ $article->id }}" tabindex="-1"
-              aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">@lang('lang.warning')!</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    @lang('lang.confirm_article_delete'){{ $article->title }}?
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">@lang('lang.no')!</button>
-                    <form action="{{ route('article.delete', [$article->id, $language->language_id]) }}" method="post">
-                      @csrf
-                      @method('delete')
-                      <input type="submit" class="btn btn-danger" value="@lang('lang.confirm')">
-                    </form>
-                  </div>
+        @forelse($documents as $document)
+          <tr>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                @if (isset($document->{'title_' . $lang_local}))
+                  {{ $document->{'title_' . $lang_local} }}
+                @else
+                  {{ __('lang.no_title') }}
+                @endif
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->format->name }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->created_at }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.show', $document->id) }}" class="text-decoration-none link-light">
+                {{ $document->updated_at }}
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.download', $document->id) }}" class="text-decoration-none link-light">
+                <i class="fa-solid fa-rotate-right text-primary"></i>
+              </a>
+            </td>
+            <td class="text-center">
+              <a href="{{ route('document.edit', $document->id) }}" class="text-decoration-none link-light">
+                <i class="fa-solid fa-rotate-right text-primary"></i>
+              </a>
+            </td>
+            <td class="text-center">
+              <div class="text-decoration-none link-light">
+                <button type="button" class="btn-unstyled" data-bs-toggle="modal"
+                  data-bs-target="#deleteModal{{ $document->id }}">
+                  {!! $icon !!}
+                </button>
+              </div>
+            </td>
+          </tr>
+          <!-- Modal -->
+          <div class="modal fade" id="deleteModal{{ $document->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">@lang('lang.warning')!</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  @lang('lang.confirm_document_delete'){{ $document->title }}?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">@lang('lang.no')!</button>
+                  <form action="{{ route('document.delete', $document->id) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" class="btn btn-danger" value="@lang('lang.confirm')">
+                  </form>
                 </div>
               </div>
-          @endforeach
-        @empty
-          <tr>@lang('lang.no_article')</tr>
+            </div>
+          @empty
+            <tr>@lang('lang.no_document')</tr>
         @endforelse
       </tbody>
     </table>
