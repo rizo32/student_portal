e8c21b7431626ea8435c202b1f03094ca8d55da5
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-04-04 21:50:14 -0400

    can't removed the other students now everybody deserves a chance!

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -18,8 +18,6 @@
           <th class="text-center text-dark">@lang('lang.name')</th>
           <th class="text-center text-dark">@lang('lang.email')</th>
           <th class="text-center text-dark">@lang('lang.birthday')</th>
-          <th class="text-center text-dark">@lang('lang.edit')</th>
-          <th class="text-center text-dark">@lang('lang.delete')</th>
         </tr>
       </thead>
       <tbody>
@@ -32,42 +30,7 @@
                 class="text-decoration-none link-light">{{ $student->user->email }}</a></td>
             <td class="text-center"><a href="{{ route('student.show', $student->user_id) }}"
                 class="text-decoration-none link-light">{{ $student->birthday }}</a></td>
-            <td class="text-center"><a href="{{ route('student.edit', $student->user_id) }}"
-                class="text-decoration-none link-light">
-                <i class="fa-solid fa-rotate-right text-primary"></i>
-              </a></td>
-            <td class="text-center">
-              <div class="text-decoration-none link-light">
-                <button type="button" class="btn-unstyled" data-bs-toggle="modal"
-                  data-bs-target="#deleteModal{{ $student->user_id }}">
-                  {!! $iconHtml !!}
-                </button>
-              </div>
-            </td>
           </tr>
-          <!-- Modal -->
-          <div class="modal fade" id="deleteModal{{ $student->user_id }}" tabindex="-1"
-            aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">@lang('lang.warning')!</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">@lang('lang.are_you_sure')
-                  {{ $student->name }}@lang('lang.student_list')?
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">@lang('lang.no')!</button>
-                  <form action="{{ route('student.delete', $student) }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" class="btn btn-danger" value="@lang('lang.confirm')">
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
 
         @empty
           <tr>@lang('lang.no_student')</tr>
